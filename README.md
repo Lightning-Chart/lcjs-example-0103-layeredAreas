8311117f4de596e41406a55e9bb1630dc15bde62
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:47:20 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Layered Areas JavaScript Chart
 
-![Layered Areas JavaScript Chart](layeredAreas.png)
+![Layered Areas JavaScript Chart](layeredAreas-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,22 +8,22 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
 
-*Also known as a Layered Area Graph, Layered Area Chart or Multiple Area Charts*
+_Also known as a Layered Area Graph, Layered Area Chart or Multiple Area Charts_
 
 Layers mean individual slices of information (series) which can be stacked to create your composition of multiple series within the same chart view using overlapping.
 
-In this example, layered area chart is basically multiple areas layered by either making use of transparency or perspective. It can be used instead of a multiple ***LineSeries*** to compare development of a trend, to search patterns and relationships both within a collection and across collections over the period.
+In this example, layered area chart is basically multiple areas layered by either making use of transparency or perspective. It can be used instead of a multiple **_LineSeries_** to compare development of a trend, to search patterns and relationships both within a collection and across collections over the period.
 
 
 ## API Links
@@ -36,23 +36,23 @@ In this example, layered area chart is basically multiple areas layered by eithe
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[XY cartesian chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html
-[Area series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html#addareaseries
+[XY cartesian chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ChartXY.html
+[Area series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ChartXY.html#addAreaSeries
 
--- src/index.js
+++ src/index.js
@@ -5,35 +5,44 @@
 const lcjs = require('@arction/lcjs')
 
 // Extract required parts from LightningChartJS.
-const {
-    lightningChart,
-    AxisTickStrategies,
-    UIElementBuilders,
-    Themes
-} = lcjs
+const { lightningChart, AxisTickStrategies, UIElementBuilders, Themes } = lcjs
 
 // Create a XY Chart.
-const xyChart = lightningChart().ChartXY({
-    // theme: Themes.darkGold 
-})
+const xyChart = lightningChart()
+    .ChartXY({
+        // theme: Themes.darkGold
+    })
     .setTitle('Product version distribution')
     .setMouseInteractions(true)
     .setPadding({ right: 25 })
 
 // Custom tick labels for X Axis.
-const reportTableXlable = ['Jan-17', 'Feb-17', 'Mar-17', 'Apr-17', 'May-17', 'Jun-17', 'Jul-17',
-    'Aug-17', 'Sep-17', 'Oct-17', 'Nov-17', 'Dec-17']
+const reportTableXlable = [
+    'Jan-17',
+    'Feb-17',
+    'Mar-17',
+    'Apr-17',
+    'May-17',
+    'Jun-17',
+    'Jul-17',
+    'Aug-17',
+    'Sep-17',
+    'Oct-17',
+    'Nov-17',
+    'Dec-17',
+]
 
 // Creating AreaSeries
 const createAreaSeries = (versionNumber, index) => {
-    return xyChart.addAreaSeries()
+    return xyChart
+        .addAreaSeries()
         .setName(`Version ${versionNumber}`)
         .setCursorResultTableFormatter((builder, series, xValue, yValue) => {
             return builder
                 .addRow(series.getName())
                 .addRow('Date: ', reportTableXlable[parseInt(xValue)])
                 .addRow('Distribution: ' + yValue.toFixed(2))
-        });
+        })
 }
 
 // Create the area series
@@ -51,319 +60,620 @@ const V11Area = createAreaSeries(11, 1)
 const V12Area = createAreaSeries(12, 2)
 
 // DataSet for all versions
-const version1 = [{
-    x: 'Jan-17', y: 1.3
-}, {
-    x: 'Feb-17', y: 1.1
-}, {
-    x: 'Mar-17', y: 1
-}, {
-    x: 'Apr-17', y: 0.8
-}, {
-    x: 'May-17', y: 0.7
-}, {
-    x: 'Jun-17', y: 0.6
-}, {
-    x: 'Jul-17', y: 0.6
-}, {
-    x: 'Aug-17', y: 0.5
-}, {
-    x: 'Sep-17', y: 0.4
-}, {
-    x: 'Oct-17', y: 0.4
-}, {
-    x: 'Nov-17', y: 0.3
-}, {
-    x: 'Dec-17', y: 0.3
-}]
+const version1 = [
+    {
+        x: 'Jan-17',
+        y: 1.3,
+    },
+    {
+        x: 'Feb-17',
+        y: 1.1,
+    },
+    {
+        x: 'Mar-17',
+        y: 1,
+    },
+    {
+        x: 'Apr-17',
+        y: 0.8,
+    },
+    {
+        x: 'May-17',
+        y: 0.7,
+    },
+    {
+        x: 'Jun-17',
+        y: 0.6,
+    },
+    {
+        x: 'Jul-17',
+        y: 0.6,
+    },
+    {
+        x: 'Aug-17',
+        y: 0.5,
+    },
+    {
+        x: 'Sep-17',
+        y: 0.4,
+    },
+    {
+        x: 'Oct-17',
+        y: 0.4,
+    },
+    {
+        x: 'Nov-17',
+        y: 0.3,
+    },
+    {
+        x: 'Dec-17',
+        y: 0.3,
+    },
+]
 
-const version2 = [{
-    x: 'Jan-17', y: 4.9
-}, {
-    x: 'Feb-17', y: 4
-}, {
-    x: 'Mar-17', y: 3.7
-}, {
-    x: 'Apr-17', y: 3.2
-}, {
-    x: 'May-17', y: 2.8
-}, {
-    x: 'Jun-17', y: 2.4
-}, {
-    x: 'Jul-17', y: 2.3
-}, {
-    x: 'Aug-17', y: 2
-}, {
-    x: 'Sep-17', y: 1.7
-}, {
-    x: 'Oct-17', y: 1.5
-}, {
-    x: 'Nov-17', y: 1.2
-}, {
-    x: 'Dec-17', y: 1.1
-}]
+const version2 = [
+    {
+        x: 'Jan-17',
+        y: 4.9,
+    },
+    {
+        x: 'Feb-17',
+        y: 4,
+    },
+    {
+        x: 'Mar-17',
+        y: 3.7,
+    },
+    {
+        x: 'Apr-17',
+        y: 3.2,
+    },
+    {
+        x: 'May-17',
+        y: 2.8,
+    },
+    {
+        x: 'Jun-17',
+        y: 2.4,
+    },
+    {
+        x: 'Jul-17',
+        y: 2.3,
+    },
+    {
+        x: 'Aug-17',
+        y: 2,
+    },
+    {
+        x: 'Sep-17',
+        y: 1.7,
+    },
+    {
+        x: 'Oct-17',
+        y: 1.5,
+    },
+    {
+        x: 'Nov-17',
+        y: 1.2,
+    },
+    {
+        x: 'Dec-17',
+        y: 1.1,
+    },
+]
 
-const version3 = [{
-    x: 'Jan-17', y: 6.8
-}, {
-    x: 'Feb-17', y: 5.9
-}, {
-    x: 'Mar-17', y: 5.4
-}, {
-    x: 'Apr-17', y: 4.6
-}, {
-    x: 'May-17', y: 4.1
-}, {
-    x: 'Jun-17', y: 3.5
-}, {
-    x: 'Jul-17', y: 3.3
-}, {
-    x: 'Aug-17', y: 3
-}, {
-    x: 'Sep-17', y: 2.6
-}, {
-    x: 'Oct-17', y: 2.2
-}, {
-    x: 'Nov-17', y: 1.8
-}, {
-    x: 'Dec-17', y: 1.5
-}]
+const version3 = [
+    {
+        x: 'Jan-17',
+        y: 6.8,
+    },
+    {
+        x: 'Feb-17',
+        y: 5.9,
+    },
+    {
+        x: 'Mar-17',
+        y: 5.4,
+    },
+    {
+        x: 'Apr-17',
+        y: 4.6,
+    },
+    {
+        x: 'May-17',
+        y: 4.1,
+    },
+    {
+        x: 'Jun-17',
+        y: 3.5,
+    },
+    {
+        x: 'Jul-17',
+        y: 3.3,
+    },
+    {
+        x: 'Aug-17',
+        y: 3,
+    },
+    {
+        x: 'Sep-17',
+        y: 2.6,
+    },
+    {
+        x: 'Oct-17',
+        y: 2.2,
+    },
+    {
+        x: 'Nov-17',
+        y: 1.8,
+    },
+    {
+        x: 'Dec-17',
+        y: 1.5,
+    },
+]
 
-const version4 = [{
-    x: 'Jan-17', y: 2
-}, {
-    x: 'Feb-17', y: 1.7
-}, {
-    x: 'Mar-17', y: 1.5
-}, {
-    x: 'Apr-17', y: 1.3
-}, {
-    x: 'May-17', y: 1.2
-}, {
-    x: 'Jun-17', y: 1
-}, {
-    x: 'Jul-17', y: 1
-}, {
-    x: 'Aug-17', y: 0.9
-}, {
-    x: 'Sep-17', y: 0.7
-}, {
-    x: 'Oct-17', y: 0.6
-}, {
-    x: 'Nov-17', y: 0.5
-}, {
-    x: 'Dec-17', y: 0.4
-}]
+const version4 = [
+    {
+        x: 'Jan-17',
+        y: 2,
+    },
+    {
+        x: 'Feb-17',
+        y: 1.7,
+    },
+    {
+        x: 'Mar-17',
+        y: 1.5,
+    },
+    {
+        x: 'Apr-17',
+        y: 1.3,
+    },
+    {
+        x: 'May-17',
+        y: 1.2,
+    },
+    {
+        x: 'Jun-17',
+        y: 1,
+    },
+    {
+        x: 'Jul-17',
+        y: 1,
+    },
+    {
+        x: 'Aug-17',
+        y: 0.9,
+    },
+    {
+        x: 'Sep-17',
+        y: 0.7,
+    },
+    {
+        x: 'Oct-17',
+        y: 0.6,
+    },
+    {
+        x: 'Nov-17',
+        y: 0.5,
+    },
+    {
+        x: 'Dec-17',
+        y: 0.4,
+    },
+]
 
-const version5 = [{
-    x: 'Jan-17', y: 25.2
-}, {
-    x: 'Feb-17', y: 22.6
-}, {
-    x: 'Mar-17', y: 20.8
-}, {
-    x: 'Apr-17', y: 18.8
-}, {
-    x: 'May-17', y: 17.1
-}, {
-    x: 'Jun-17', y: 15.1
-}, {
-    x: 'Jul-17', y: 14.5
-}, {
-    x: 'Aug-17', y: 13.4
-}, {
-    x: 'Sep-17', y: 12
-}, {
-    x: 'Oct-17', y: 10.3
-}, {
-    x: 'Nov-17', y: 8.6
-}, {
-    x: 'Dec-17', y: 7.6
-}]
+const version5 = [
+    {
+        x: 'Jan-17',
+        y: 25.2,
+    },
+    {
+        x: 'Feb-17',
+        y: 22.6,
+    },
+    {
+        x: 'Mar-17',
+        y: 20.8,
+    },
+    {
+        x: 'Apr-17',
+        y: 18.8,
+    },
+    {
+        x: 'May-17',
+        y: 17.1,
+    },
+    {
+        x: 'Jun-17',
+        y: 15.1,
+    },
+    {
+        x: 'Jul-17',
+        y: 14.5,
+    },
+    {
+        x: 'Aug-17',
+        y: 13.4,
+    },
+    {
+        x: 'Sep-17',
+        y: 12,
+    },
+    {
+        x: 'Oct-17',
+        y: 10.3,
+    },
+    {
+        x: 'Nov-17',
+        y: 8.6,
+    },
+    {
+        x: 'Dec-17',
+        y: 7.6,
+    },
+]
 
-const version6 = [{
-    x: 'Jan-17', y: 11.3
-}, {
-    x: 'Feb-17', y: 10.1
-}, {
-    x: 'Mar-17', y: 9.4
-}, {
-    x: 'Apr-17', y: 8.7
-}, {
-    x: 'May-17', y: 7.8
-}, {
-    x: 'Jun-17', y: 7.1
-}, {
-    x: 'Jul-17', y: 6.7
-}, {
-    x: 'Aug-17', y: 6.1
-}, {
-    x: 'Sep-17', y: 5.4
-}, {
-    x: 'Oct-17', y: 4.8
-}, {
-    x: 'Nov-17', y: 3.8
-}, {
-    x: 'Dec-17', y: 3.5
-}]
+const version6 = [
+    {
+        x: 'Jan-17',
+        y: 11.3,
+    },
+    {
+        x: 'Feb-17',
+        y: 10.1,
+    },
+    {
+        x: 'Mar-17',
+        y: 9.4,
+    },
+    {
+        x: 'Apr-17',
+        y: 8.7,
+    },
+    {
+        x: 'May-17',
+        y: 7.8,
+    },
+    {
+        x: 'Jun-17',
+        y: 7.1,
+    },
+    {
+        x: 'Jul-17',
+        y: 6.7,
+    },
+    {
+        x: 'Aug-17',
+        y: 6.1,
+    },
+    {
+        x: 'Sep-17',
+        y: 5.4,
+    },
+    {
+        x: 'Oct-17',
+        y: 4.8,
+    },
+    {
+        x: 'Nov-17',
+        y: 3.8,
+    },
+    {
+        x: 'Dec-17',
+        y: 3.5,
+    },
+]
 
-const version7 = [{
-    x: 'Jan-17', y: 22.8
-}, {
-    x: 'Feb-17', y: 23.3
-}, {
-    x: 'Mar-17', y: 23.1
-}, {
-    x: 'Apr-17', y: 23.3
-}, {
-    x: 'May-17', y: 23.3
-}, {
-    x: 'Jun-17', y: 21.7
-}, {
-    x: 'Jul-17', y: 21
-}, {
-    x: 'Aug-17', y: 20.2
-}, {
-    x: 'Sep-17', y: 19.2
-}, {
-    x: 'Oct-17', y: 17.6
-}, {
-    x: 'Nov-17', y: 15.4
-}, {
-    x: 'Dec-17', y: 14.4
-}]
+const version7 = [
+    {
+        x: 'Jan-17',
+        y: 22.8,
+    },
+    {
+        x: 'Feb-17',
+        y: 23.3,
+    },
+    {
+        x: 'Mar-17',
+        y: 23.1,
+    },
+    {
+        x: 'Apr-17',
+        y: 23.3,
+    },
+    {
+        x: 'May-17',
+        y: 23.3,
+    },
+    {
+        x: 'Jun-17',
+        y: 21.7,
+    },
+    {
+        x: 'Jul-17',
+        y: 21,
+    },
+    {
+        x: 'Aug-17',
+        y: 20.2,
+    },
+    {
+        x: 'Sep-17',
+        y: 19.2,
+    },
+    {
+        x: 'Oct-17',
+        y: 17.6,
+    },
+    {
+        x: 'Nov-17',
+        y: 15.4,
+    },
+    {
+        x: 'Dec-17',
+        y: 14.4,
+    },
+]
 
-const version8 = [{
-    x: 'Jan-17', y: 24
-}, {
-    x: 'Feb-17', y: 29.6
-}, {
-    x: 'Mar-17', y: 31.3
-}, {
-    x: 'Apr-17', y: 31.2
-}, {
-    x: 'May-17', y: 31.8
-}, {
-    x: 'Jun-17', y: 32.2
-}, {
-    x: 'Jul-17', y: 32
-}, {
-    x: 'Aug-17', y: 29.7
-}, {
-    x: 'Sep-17', y: 28.1
-}, {
-    x: 'Oct-17', y: 25.5
-}, {
-    x: 'Nov-17', y: 22.7
-}, {
-    x: 'Dec-17', y: 21.3
-}]
+const version8 = [
+    {
+        x: 'Jan-17',
+        y: 24,
+    },
+    {
+        x: 'Feb-17',
+        y: 29.6,
+    },
+    {
+        x: 'Mar-17',
+        y: 31.3,
+    },
+    {
+        x: 'Apr-17',
+        y: 31.2,
+    },
+    {
+        x: 'May-17',
+        y: 31.8,
+    },
+    {
+        x: 'Jun-17',
+        y: 32.2,
+    },
+    {
+        x: 'Jul-17',
+        y: 32,
+    },
+    {
+        x: 'Aug-17',
+        y: 29.7,
+    },
+    {
+        x: 'Sep-17',
+        y: 28.1,
+    },
+    {
+        x: 'Oct-17',
+        y: 25.5,
+    },
+    {
+        x: 'Nov-17',
+        y: 22.7,
+    },
+    {
+        x: 'Dec-17',
+        y: 21.3,
+    },
+]
 
-const version9 = [{
-    x: 'Jan-17', y: 0.3
-}, {
-    x: 'Feb-17', y: 0.5
-}, {
-    x: 'Mar-17', y: 2.4
-}, {
-    x: 'Apr-17', y: 6.6
-}, {
-    x: 'May-17', y: 10.6
-}, {
-    x: 'Jun-17', y: 14.2
-}, {
-    x: 'Jul-17', y: 15.8
-}, {
-    x: 'Aug-17', y: 19.3
-}, {
-    x: 'Sep-17', y: 22.3
-}, {
-    x: 'Oct-17', y: 22.9
-}, {
-    x: 'Nov-17', y: 20.3
-}, {
-    x: 'Dec-17', y: 18.1
-}]
+const version9 = [
+    {
+        x: 'Jan-17',
+        y: 0.3,
+    },
+    {
+        x: 'Feb-17',
+        y: 0.5,
+    },
+    {
+        x: 'Mar-17',
+        y: 2.4,
+    },
+    {
+        x: 'Apr-17',
+        y: 6.6,
+    },
+    {
+        x: 'May-17',
+        y: 10.6,
+    },
+    {
+        x: 'Jun-17',
+        y: 14.2,
+    },
+    {
+        x: 'Jul-17',
+        y: 15.8,
+    },
+    {
+        x: 'Aug-17',
+        y: 19.3,
+    },
+    {
+        x: 'Sep-17',
+        y: 22.3,
+    },
+    {
+        x: 'Oct-17',
+        y: 22.9,
+    },
+    {
+        x: 'Nov-17',
+        y: 20.3,
+    },
+    {
+        x: 'Dec-17',
+        y: 18.1,
+    },
+]
 
-const version10 = [{
-    x: 'Jan-17', y: 0
-}, {
-    x: 'Feb-17', y: 0.2
-}, {
-    x: 'Mar-17', y: 0.4
-}, {
-    x: 'Apr-17', y: 0.5
-}, {
-    x: 'May-17', y: 0.9
-}, {
-    x: 'Jun-17', y: 1.6
-}, {
-    x: 'Jul-17', y: 2
-}, {
-    x: 'Aug-17', y: 4
-}, {
-    x: 'Sep-17', y: 6.2
-}, {
-    x: 'Oct-17', y: 8.2
-}, {
-    x: 'Nov-17', y: 10.5
-}, {
-    x: 'Dec-17', y: 10.1
-}]
+const version10 = [
+    {
+        x: 'Jan-17',
+        y: 0,
+    },
+    {
+        x: 'Feb-17',
+        y: 0.2,
+    },
+    {
+        x: 'Mar-17',
+        y: 0.4,
+    },
+    {
+        x: 'Apr-17',
+        y: 0.5,
+    },
+    {
+        x: 'May-17',
+        y: 0.9,
+    },
+    {
+        x: 'Jun-17',
+        y: 1.6,
+    },
+    {
+        x: 'Jul-17',
+        y: 2,
+    },
+    {
+        x: 'Aug-17',
+        y: 4,
+    },
+    {
+        x: 'Sep-17',
+        y: 6.2,
+    },
+    {
+        x: 'Oct-17',
+        y: 8.2,
+    },
+    {
+        x: 'Nov-17',
+        y: 10.5,
+    },
+    {
+        x: 'Dec-17',
+        y: 10.1,
+    },
+]
 
-const version11 = [{
-    x: 'Jan-17', y: 0
-}, {
-    x: 'Feb-17', y: 0
-}, {
-    x: 'Mar-17', y: 0
-}, {
-    x: 'Apr-17', y: 0
-}, {
-    x: 'May-17', y: 0
-}, {
-    x: 'Jun-17', y: 0
-}, {
-    x: 'Jul-17', y: 0.2
-}, {
-    x: 'Aug-17', y: 0.5
-}, {
-    x: 'Sep-17', y: 0.8
-}, {
-    x: 'Oct-17', y: 4.9
-}, {
-    x: 'Nov-17', y: 11.4
-}, {
-    x: 'Dec-17', y: 14
-}]
+const version11 = [
+    {
+        x: 'Jan-17',
+        y: 0,
+    },
+    {
+        x: 'Feb-17',
+        y: 0,
+    },
+    {
+        x: 'Mar-17',
+        y: 0,
+    },
+    {
+        x: 'Apr-17',
+        y: 0,
+    },
+    {
+        x: 'May-17',
+        y: 0,
+    },
+    {
+        x: 'Jun-17',
+        y: 0,
+    },
+    {
+        x: 'Jul-17',
+        y: 0.2,
+    },
+    {
+        x: 'Aug-17',
+        y: 0.5,
+    },
+    {
+        x: 'Sep-17',
+        y: 0.8,
+    },
+    {
+        x: 'Oct-17',
+        y: 4.9,
+    },
+    {
+        x: 'Nov-17',
+        y: 11.4,
+    },
+    {
+        x: 'Dec-17',
+        y: 14,
+    },
+]
 
-const version12 = [{
-    x: 'Jan-17', y: 0
-}, {
-    x: 'Feb-17', y: 0
-}, {
-    x: 'Mar-17', y: 0
-}, {
-    x: 'Apr-17', y: 0
-}, {
-    x: 'May-17', y: 0
-}, {
-    x: 'Jun-17', y: 0
-}, {
-    x: 'Jul-17', y: 0
-}, {
-    x: 'Aug-17', y: 0
-}, {
-    x: 'Sep-17', y: 0.3
-}, {
-    x: 'Oct-17', y: 0.8
-}, {
-    x: 'Nov-17', y: 3.2
-}, {
-    x: 'Dec-17', y: 7.5
-}]
+const version12 = [
+    {
+        x: 'Jan-17',
+        y: 0,
+    },
+    {
+        x: 'Feb-17',
+        y: 0,
+    },
+    {
+        x: 'Mar-17',
+        y: 0,
+    },
+    {
+        x: 'Apr-17',
+        y: 0,
+    },
+    {
+        x: 'May-17',
+        y: 0,
+    },
+    {
+        x: 'Jun-17',
+        y: 0,
+    },
+    {
+        x: 'Jul-17',
+        y: 0,
+    },
+    {
+        x: 'Aug-17',
+        y: 0,
+    },
+    {
+        x: 'Sep-17',
+        y: 0.3,
+    },
+    {
+        x: 'Oct-17',
+        y: 0.8,
+    },
+    {
+        x: 'Nov-17',
+        y: 3.2,
+    },
+    {
+        x: 'Dec-17',
+        y: 7.5,
+    },
+]
 
-const axisX = xyChart.getDefaultAxisX()
+const axisX = xyChart
+    .getDefaultAxisX()
     .setMouseInteractions(false)
     .setScrollStrategy(undefined)
     // Disable default ticks.
@@ -371,23 +681,21 @@ const axisX = xyChart.getDefaultAxisX()
 
 // Create Custom Axis
 let customAxisX = (data, index) => {
-    axisX.addCustomTick(UIElementBuilders.AxisTick)
+    axisX
+        .addCustomTick()
         .setValue(index)
         .setGridStrokeLength(0)
         .setTextFormatter((_) => data[index].x)
-
 }
 
 // Generate Chart
 const generateChart = (data, area, createTicks) => {
-    data
-        .map((p, index) => ({ x: index, y: p.y }))
-        .forEach((point, index) => {
-            area.add(point)
-            if(createTicks){
-                customAxisX(data, index)
-            }
-        })
+    data.map((p, index) => ({ x: index, y: p.y })).forEach((point, index) => {
+        area.add(point)
+        if (createTicks) {
+            customAxisX(data, index)
+        }
+    })
 }
 
 // generating different layered for charts
@@ -405,13 +713,11 @@ generateChart(version11, V11Area)
 generateChart(version12, V12Area)
 
 // Enable AutoCursor auto-fill.
-xyChart.setAutoCursor((cursor) => cursor
-    .setResultTableAutoTextStyle(false)
-    .disposeTickMarkerX()
-)
+xyChart.setAutoCursor((cursor) => cursor.setResultTableAutoTextStyle(false).setTickMarkerXVisible(false))
 
-xyChart.getDefaultAxisY()
+xyChart
+    .getDefaultAxisY()
     .setTitle('Percentage')
     .setScrollStrategy(undefined)
-    .setInterval(0, 50)
+    .setInterval({ start: 0, end: 50, stopAxisAfter: false })
     .setMouseInteractions(false)
